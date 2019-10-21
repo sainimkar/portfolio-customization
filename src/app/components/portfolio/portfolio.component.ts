309adad94bbf96da328d09efba5f924ead5fad9a
SaiNimkar <nimkarsai@gmail.com>
2019-10-22 04:14:02 +0530

    static piechart rendered

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from 'src/assets/canvasjs.min';
 
 @Component({
   selector: 'app-portfolio',
@@ -10,6 +11,32 @@ export class PortfolioComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+      
+    let chart = new CanvasJS.Chart("chartContainer", {
+      theme: "light2",
+      animationEnabled: true,
+      exportEnabled: false,
+      title:{
+        text: "Monthly Expense"
+      },
+      data: [{
+        type: "pie",
+        showInLegend: true,
+        toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+        indexLabel: "{name} - #percent%",
+        dataPoints: [
+          { y: 450, name: "Food" },
+          { y: 120, name: "Insurance" },
+          { y: 300, name: "Traveling" },
+          { y: 800, name: "Housing" },
+          { y: 150, name: "Education" },
+          { y: 150, name: "Shopping"},
+          { y: 250, name: "Others" }
+        ]
+      }]
+    });
+      
+    chart.render();
   }
 
 }
--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -32,5 +32,6 @@ export class ServicesComponent implements OnInit {
   }
 
   ngOnInit(){}
-
 }
+
+
