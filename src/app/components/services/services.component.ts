335b98bd625b2ae15af4d023f1d6c04267724cc9
SaiNimkar <nimkarsai@gmail.com>
2019-11-03 18:06:27 +0530

    naviagating to portfolio

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { CosmicServiceService } from './../../services/cosmic-service.service';
 import { FormsModule, FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 import { User } from 'src/app/user';
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -14,7 +15,7 @@ export class ServicesComponent implements OnInit {
 
   // tslint:disable-next-line: typedef-whitespace
   // tslint:disable-next-line: variable-name
-  constructor(private _service: CosmicServiceService){}
+  constructor(private _service: CosmicServiceService, private router: Router){}
 
 
   userModel = new User( 20 , 1,  15000, 5000, 5000);
@@ -27,6 +28,8 @@ export class ServicesComponent implements OnInit {
             data => console.log('success', data),
             error => console.error('Error!', error )
         );
+
+        this.router.navigate(['/portfolio']);
   }
 
   ngOnInit(){}
